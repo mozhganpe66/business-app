9e9cab5e66f2c6bd712449fbaa2138cac17af5eb
mozhganpe66 <mozhgan.pe66@gmail.com>
2026-02-06 13:42:42 +0100

    changes links

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -36,12 +36,12 @@ export default function HomePage(){
                       />  
                        <h4>Finance</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Budgeting</a></li>
-                        <li><a href="#">Personal finance advice</a></li>
-                        <li><a href="#">Financial planning</a></li>
-                        <li><a href="#">Excel & spreadsheets</a></li>
-                        <li><a href="#">Expense tracking</a></li>
-                        <li><a href="#">Financial analysis (basic)</a></li>
+                        <li><a href="Tasks">Budgeting</a></li>
+                        <li><a href="Tasks">Personal finance advice</a></li>
+                        <li><a href="Tasks">Financial planning</a></li>
+                        <li><a href="Tasks">Excel & spreadsheets</a></li>
+                        <li><a href="Tasks">Expense tracking</a></li>
+                        <li><a href="Tasks">Financial analysis (basic)</a></li>
 
                        </ul>
                     </Col>
@@ -53,12 +53,12 @@ export default function HomePage(){
                       />  
                        <h4>Accounting</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Bookkeeping</a></li>
-                        <li><a href="#">Tax reporting(basic)</a></li>
-                        <li><a href="#">Invoicing</a></li>
-                        <li><a href="#">Payroll(basic)</a></li>
-                        <li><a href="#">Financial statements</a></li>
-                        <li><a href="#">Startup accounting help</a></li>
+                        <li><a href="Tasks">Bookkeeping</a></li>
+                        <li><a href="Tasks">Tax reporting(basic)</a></li>
+                        <li><a href="Tasks">Invoicing</a></li>
+                        <li><a href="Tasks">Payroll(basic)</a></li>
+                        <li><a href="Tasks">Financial statements</a></li>
+                        <li><a href="Tasks">Startup accounting help</a></li>
                        </ul>
                     </Col>
                     
@@ -69,12 +69,12 @@ export default function HomePage(){
                       />  
                        <h4>IT</h4> 
                         <ul className='p-0'>
-                        <li><a href="#">IT support / troubleshooting</a></li>
+                        <li><a href="Tasks">IT support / troubleshooting</a></li>
                         <li><a href="Tasks">Web development</a></li>
-                        <li><a href="#">App testing</a></li>
-                        <li><a href="#">Basic programming help</a></li>
-                        <li><a href="#">Data handling</a></li>
-                        <li><a href="#">System setup</a></li>
+                        <li><a href="Tasks">App testing</a></li>
+                        <li><a href="Tasks">Basic programming help</a></li>
+                        <li><a href="Tasks">Data handling</a></li>
+                        <li><a href="Tasks">System setup</a></li>
                        </ul>
                     </Col> 
                 </Row>
@@ -85,12 +85,12 @@ export default function HomePage(){
                       />  
                        <h4>Cooking</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Home cooking</a></li>
-                        <li><a href="#">Meal prep</a></li>
-                        <li><a href="#">Baking</a></li>
-                        <li><a href="#">Catering help</a></li>
-                        <li><a href="#">Recipe development</a></li>
-                        <li><a href="#">Cultural / international cuisine</a></li>
+                        <li><a href="Tasks">Home cooking</a></li>
+                        <li><a href="Tasks">Meal prep</a></li>
+                        <li><a href="Tasks">Baking</a></li>
+                        <li><a href="Tasks">Catering help</a></li>
+                        <li><a href="Tasks">Recipe development</a></li>
+                        <li><a href="Tasks">Cultural / international cuisine</a></li>
                        </ul>
                        </Col>
                     <Col lg={true}>
@@ -100,12 +100,12 @@ export default function HomePage(){
                       />  
                        <h4>Media and Marketing</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Social media management</a></li>
-                        <li><a href="#">Content creation</a></li>
-                        <li><a href="#">Copywriting</a></li>
-                        <li><a href="#">SEO basics</a></li>
-                        <li><a href="#">Email marketing</a></li>
-                        <li><a href="#">Brand strategy (basic)</a></li>
+                        <li><a href="Tasks">Social media management</a></li>
+                        <li><a href="Tasks">Content creation</a></li>
+                        <li><a href="Tasks">Copywriting</a></li>
+                        <li><a href="Tasks">SEO basics</a></li>
+                        <li><a href="Tasks">Email marketing</a></li>
+                        <li><a href="Tasks">Brand strategy (basic)</a></li>
 
                        </ul>
                        </Col>
@@ -116,12 +116,12 @@ export default function HomePage(){
                       />  
                        <h4>Healthcare</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Healthcare assistance</a></li>
-                        <li><a href="#">Medical administration help</a></li>
-                        <li><a href="#">Patient support</a></li>
-                        <li><a href="#">Health education</a></li>
-                        <li><a href="#">Research assistance</a></li>
-                        <li><a href="#">Wellness Support</a></li>
+                        <li><a href="Tasks">Healthcare assistance</a></li>
+                        <li><a href="Tasks">Medical administration help</a></li>
+                        <li><a href="Tasks">Patient support</a></li>
+                        <li><a href="Tasks">Health education</a></li>
+                        <li><a href="Tasks">Research assistance</a></li>
+                        <li><a href="Tasks">Wellness Support</a></li>
                        </ul>
                        </Col> 
                 </Row>
@@ -132,12 +132,12 @@ export default function HomePage(){
                       />  
                        <h4>Law</h4>
                        <ul className='p-0'>
-                        <li><a href="#">Legal Research</a></li>
-                        <li><a href="#">Contract review (basic)</a></li>
-                        <li><a href="#">Document drafting</a></li>
-                        <li><a href="#">Compliance help</a></li>
-                        <li><a href="#">Case preparation</a></li>
-                        <li><a href="#">Legal translation</a></li>
+                        <li><a href="Taks">Legal Research</a></li>
+                        <li><a href="Tasks">Contract review (basic)</a></li>
+                        <li><a href="Tasks">Document drafting</a></li>
+                        <li><a href="Tasks">Compliance help</a></li>
+                        <li><a href="Tasks">Case preparation</a></li>
+                        <li><a href="Tasks">Legal translation</a></li>
                        </ul>
                        </Col>
                     <Col lg={true}>
@@ -147,12 +147,12 @@ export default function HomePage(){
                       />  
                        <h4>Music and Dance</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Music lessons</a></li>
-                        <li><a href="#">Dance lessons</a></li>
-                        <li><a href="#">Choreography</a></li>
-                        <li><a href="#">Instrument practice</a></li>
-                        <li><a href="#">Music Theory</a></li>
-                        <li><a href="#">Performance coaching</a></li>
+                        <li><a href="Tasks">Music lessons</a></li>
+                        <li><a href="Tasks">Dance lessons</a></li>
+                        <li><a href="Tasks">Choreography</a></li>
+                        <li><a href="Tasks">Instrument practice</a></li>
+                        <li><a href="Tasks">Music Theory</a></li>
+                        <li><a href="Tasks">Performance coaching</a></li>
                        </ul>
                        </Col>
                     <Col lg={true}>
@@ -162,12 +162,12 @@ export default function HomePage(){
                       />  
                        <h4>Translations</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Document translation</a></li>
-                        <li><a href="#">Website translation</a></li>
-                        <li><a href="#">Proofreading</a></li>
-                        <li><a href="#">Localization</a></li>
-                        <li><a href="#">Academic translation</a></li>
-                        <li><a href="#">Business translation</a></li>
+                        <li><a href="Tasks">Document translation</a></li>
+                        <li><a href="Tasks">Website translation</a></li>
+                        <li><a href="Tasks">Proofreading</a></li>
+                        <li><a href="Tasks">Localization</a></li>
+                        <li><a href="Tasks">Academic translation</a></li>
+                        <li><a href="Tasks">Business translation</a></li>
                        </ul>
                        </Col> 
                 </Row> 
@@ -178,12 +178,12 @@ export default function HomePage(){
                       />  
                        <h4>Tutoring</h4>
                        <ul className='p-0'>
-                        <li><a href="#">Language tutoring</a></li>
-                        <li><a href="#">Math tutoring</a></li>
-                        <li><a href="#">Physics tutoring</a></li>
-                        <li><a href="#">Economics tutoring</a></li>
-                        <li><a href="#">Psychology tutoring</a></li>
-                        <li><a href="#">Exam Preparation</a></li>
+                        <li><a href="Tasks">Language tutoring</a></li>
+                        <li><a href="Tasks">Math tutoring</a></li>
+                        <li><a href="Tasks">Physics tutoring</a></li>
+                        <li><a href="Tasks">Economics tutoring</a></li>
+                        <li><a href="Tasks">Psychology tutoring</a></li>
+                        <li><a href="Tasks">Exam Preparation</a></li>
                         
                        </ul>
                        
@@ -195,12 +195,12 @@ export default function HomePage(){
                       />  
                        <h4>Physiotherapy (support only)</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Exercise guidance</a></li>
-                        <li><a href="#">Rehabilitation support</a></li>
-                        <li><a href="#">Posture correction</a></li>
-                        <li><a href="#">Injury prevention</a></li>
-                        <li><a href="#">Stretching programs</a></li>
-                        <li><a href="#">Mobility Training</a></li>
+                        <li><a href="Tasks">Exercise guidance</a></li>
+                        <li><a href="Tasks">Rehabilitation support</a></li>
+                        <li><a href="Tasks">Posture correction</a></li>
+                        <li><a href="Tasks">Injury prevention</a></li>
+                        <li><a href="Tasks">Stretching programs</a></li>
+                        <li><a href="Tasks">Mobility Training</a></li>
                        </ul>
                        </Col>
                     <Col lg={true}>
@@ -210,12 +210,13 @@ export default function HomePage(){
                       />  
                        <h4>Repairing</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Home repairs</a></li>
-                        <li><a href="#">Furniture assembly</a></li>
-                        <li><a href="#">Plumbing help (basic)</a></li>
-                        <li><a href="#">Painting & decoration</a></li>
-                        <li><a href="#">Installation help</a></li>
-                         <li><a href="#">Maintenance tasks</a></li>
+                        <li><a href="Tasks">Home repairs</a></li>
+                        <li><a href="Tasks">Furniture assembly</a></li>
+                        <li><a href="Tasks">Plumbing help (basic)</a></li>
+                        <li><a href="Tasks">Painting & decoration</a></li>
+                        <li><a href="Tasks">Installation help</a></li>
+                         <li><a href="Tasks
+                         ">Maintenance tasks</a></li>
                        </ul>
                        </Col> 
                 </Row>
@@ -226,12 +227,12 @@ export default function HomePage(){
                       />  
                        <h4>Electronics</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Device repair</a></li>
-                        <li><a href="#">Phone repair</a></li>
-                        <li><a href="#">Computer repair</a></li>
-                        <li><a href="#">Hardware Installation</a></li>
-                        <li><a href="#">Smart home setup</a></li>
-                        <li><a href="#">Electronics troubleshooting</a></li>
+                        <li><a href="Tasks">Device repair</a></li>
+                        <li><a href="Tasks">Phone repair</a></li>
+                        <li><a href="Tasks">Computer repair</a></li>
+                        <li><a href="Tasks">Hardware Installation</a></li>
+                        <li><a href="Tasks">Smart home setup</a></li>
+                        <li><a href="Tasks">Electronics troubleshooting</a></li>
                        </ul>
                        </Col>
                     <Col lg={true}>
@@ -241,12 +242,12 @@ export default function HomePage(){
                       />  
                        <h4>Art and Design</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Illustration</a></li>
-                        <li><a href="#">Digital art</a></li>
-                        <li><a href="#">Painting</a></li>
-                        <li><a href="#">UI design</a></li>
-                        <li><a href="#">UX design</a></li>
-                        <li><a href="#">Animation (basic)</a></li>
+                        <li><a href="Tasks">Illustration</a></li>
+                        <li><a href="Tasks">Digital art</a></li>
+                        <li><a href="Tasks">Painting</a></li>
+                        <li><a href="Tasks">UI design</a></li>
+                        <li><a href="Tasks">UX design</a></li>
+                        <li><a href="Thasks">Animation (basic)</a></li>
                        </ul>
                        </Col>
                     <Col lg={true}>
@@ -256,12 +257,13 @@ export default function HomePage(){
                       />  
                        <h4>Graphic design</h4>
                         <ul className='p-0'>
-                        <li><a href="#">Logo design</a></li>
-                        <li><a href="#">Social media graphics</a></li>
-                        <li><a href="#">Posters & flyers</a></li>
-                        <li><a href="#">Business cards</a></li>
-                        <li><a href="#">Presentations</a></li>
-                        <li><a href="#">Branding materials</a></li>
+                        <li><a href="Tasks">Logo design</a></li>
+                        <li><a href="Tasks">Social media graphics</a></li>
+                        <li><a href="Tasks">Posters & flyers</a></li>
+                        <li><a href="Tasks">Business cards</a></li>
+                        <li><a href="Tasks">Presentations</a></li>
+                        <li><a href="Thaks
+                        ">Branding materials</a></li>
                        </ul>
                        </Col> 
                 </Row>
